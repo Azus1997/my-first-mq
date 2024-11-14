f61ff894bd50e5f8ce003d98af1b5caf73ea659c
Matias Petite <accicgem.prism039@passinbox.com>
2024-11-14 20:00:30 -0300

    my-first-mq: created publisher (football-match)
    - created football match
    - initialized Dockerfile

---
+++ football-match/src/FootballMatchApp.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import random
+import time
+import asyncio
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost')) #change localhost for IP address in case of remote connection
+channel = connection.channel()
+
+channel.queue_declare(queue='football-match')
+
+goalChance = 0
+
+goalCount = 0
+
+oTime = time.time()
+
+spentTime = time.time() - oTime
+
+
+while int(spentTime) <= 15:
+    goalChance = goalChance  + 7
+    n = random.randint(0, 100) 
+    if n < goalChance:
+        goalCount = goalCount + 1
+        goalChance = 0
+        channel.basic_publish(exchange='',
+                      routing_key='football-match',
+                      body=('Goal!' + str(goalCount)))
+                      
+    time.sleep(1)
+
+    spentTime = time.time() - oTime
+
+channel.basic_publish(exchange='',
+                      routing_key='football-match',
+                      body=('EOM'))
+connection.close()
